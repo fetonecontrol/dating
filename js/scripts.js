4e729de956612734ebc62b83583f36df80f5894d
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-04 15:12:31 -0700

    intialize directories
    co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
     if (gender === 'male' && attribute === 'athletic') {
       let choice = "Arnold Schwartzenegger";
       $(".choice").text(choice);
-    } else if (gender === 'female' && attribute === 'athletic') {
-      let choice = "Arnold Schwartzenegger";
+    } else if (gender === 'male' && attribute === 'caring') {
+      let choice = "Brad Pitt";
       $(".choice").text(choice);
     } else if (gender === 'male' && attribute === 'rich') {
       let choice = "Bill Gates";
       $(".choice").text(choice);
-    } else if (gender === 'female' && attribute === 'itelligent') {
+    } else if (gender === 'female' && attribute === 'intelligent') {
       let choice = "Madeleine Albright";
       $(".choice").text(choice);
     }
